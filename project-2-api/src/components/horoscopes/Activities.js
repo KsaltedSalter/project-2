e7d88ab30e89eccd866b828f0d3953364abb8727
James Marrs <jamesmarrs@jmpersoallaptop.home>
2021-09-04 12:20:20 +0100

    activities page tidied up

--- project-2-api/src/components/horoscopes/Activities.js
+++ project-2-api/src/components/horoscopes/Activities.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Activites = () => {
-  const [NEWS, setNEWS] = React.useState([]);
-  const [NEWSimage, setNEWSimage] = React.useState([]);
+  const [NEWS, setNEWS] = React.useState([])
+
   //wrap axios in button
 
-  const json = localStorage.getItem('horoscope');
-  const horoscopeInfo = JSON.parse(json);
-  console.log(horoscopeInfo);
+  const json = localStorage.getItem('horoscope')
+  const horoscopeInfo = JSON.parse(json)
+  //  console.log(horoscopeInfo)
 
   // useEffect(() => {
   //   axios.get('ENDPOINT)
@@ -23,7 +24,7 @@ const Activites = () => {
     params: {
       q: `${horoscopeInfo.color} ${horoscopeInfo.lucky_number} ${horoscopeInfo.lucky_time} ${horoscopeInfo.mood} `,
       pageNumber: '1',
-      pageSize: '3',
+      pageSize: '10',
       autoCorrect: 'true',
       fromPublishedDate: 'null',
       toPublishedDate: 'null',
@@ -32,46 +33,52 @@ const Activites = () => {
       'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
       'x-rapidapi-key': 'eb499342e9mshbdc31337851737ep193f5ajsn3bbb2c7e520c',
     },
-  };
+  }
 
   useEffect(() => {
     axios
       .request(options)
       .then(function (response) {
-        const newsBody = response.data.value[0]; //single news piece
-        //const newsBody = response.data.value //try map through array
-        setNEWS(newsBody);
-        console.log(newsBody);
-        setNEWSimage(newsBody.image);
+        //        console.log(response.data.value)
+        //const newsBody = response.data.value[0] //single news piece
+        const newsBody = response.data.value //try map through array
+        setNEWS(newsBody)
+        console.log(newsBody)
+        //        console.log(newsBody)
       })
       .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
+        console.error(error)
+      })
+  }, [])
 
   return (
     <>
-      {NEWS.url ? (
-        <div>
-          {/* {NEWS.map((NEW) => (
-            <a href={NEW.url}>
-              <h1 className='title is-1'>{NEW.title}</h1>
-            </a>
-          ))} */}
-          <a href={NEWS.url}>
-            <h1 className="title is-1">{NEWS.title}</h1>
-            <img src={NEWSimage.url} alt={NEWS.title}></img>
-            <h2 className="title is-2">{NEWS.description}</h2>
-          </a>
+      {NEWS[0] ? (
+        <div className='container mt-4'>
+          {NEWS.map((NEW) => (
+            <div className='has-text-centered'>
+              <a href={NEW.url}>
+                <h1 className='title is-3'>{NEW.title}</h1>
+                <img src={NEW.image.url} alt={NEW.title} max-height='50'></img>
+              </a>
+              <h2 className='subtitle is-4 is-italic'>{NEW.description}</h2>
+              <hr />
+            </div>
+          ))}
+          <div className='has-text-centered mb-4'>
+            <Link to={'/today'} className='button is-link is-rounded'>
+              <p>Back to your horoscope</p>
+            </Link>{' '}
+          </div>
         </div>
       ) : (
         <p>Loading news stories...</p>
       )}
     </>
-  );
-};
+  )
+}
 
-export default Activites;
+export default Activites
 
 //
 //429
